af9d55837304971ac0b5507b470269b6dc4adebd
linuxgnuconvert <spencer.herzberg@gmail.com>
2010-05-16 02:00:59 -0400

    Modified Deacon.java to contain Android BroadcastReceiver to allow for easy "push notification" of when the network is up or down. Placed stubs in the code for where starting and stopping the DeaconService should happen

--- src/org/deacon/Deacon.java
+++ src/org/deacon/Deacon.java
@@ -3,9 +3,13 @@ package org.deacon;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
@@ -38,7 +42,19 @@ public class Deacon extends DeaconService {
 			Log.d("Deacon","Intent Received");
 			if (intent.getAction().equals(android.net.ConnectivityManager.CONNECTIVITY_ACTION)) {
 				Log.d(Deacon.class.getSimpleName(), "action: " + intent.getAction());
-				
+				ConnectivityManager connman = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+				NetworkInfo netinfo = connman.getActiveNetworkInfo();
+				if (netinfo != null){//seems if network is disconnected, netinfo is null?
+					if (netinfo.isConnected()){
+						Log.d("Deacon","Network has connection!");
+					}
+					else{
+						Log.d("Deacon","Network is disconnected!");//Not sure this will ever happen...
+					}
+				}
+				else{
+					Log.d("Deacon","Network is disconnected!");
+				}
 		    }
 
 			
@@ -46,13 +62,25 @@ public class Deacon extends DeaconService {
 	};
 	
 	/**
+	 * Method to return the broadcast receiver, you must register 
+	 * this BroadcastReceiver from an Activity.
+	 */
+	public BroadcastReceiver getBroadcastReceiver(){
+		return bcr;
+	}
+	
+	
+	/**
 	 * @param host The Meteor server to which this client should connect
 	 * @param port The client port that the Meteor server is listening on (default is usually 4670) 
+	 * @param activity The activity that creates this class. Used to register an IntentFilter.//Not a great idea...
 	 * @throws UnknownHostException
 	 * @throws IOException
 	 */
-	public Deacon(String host, int port) throws UnknownHostException, IOException {
+	public Deacon(String host, int port, Activity activity) throws UnknownHostException, IOException {
 		super(host, port);
+		activity.registerReceiver(this.getBroadcastReceiver(), new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
+		
 	}
 	
 	/**
